98e15f61259f511fa8aaa7aaebaf6734c3de388f
AhmedSalah959 <as.yuri74@gmail.com>
2019-04-14 21:12:34 +0200

    -_-

--- controllers/user.js
+++ controllers/user.js
@@ -141,7 +141,7 @@ exports.User_profile = (req, res, next) => {
                 post: result.post,
                 event: result.event,
                 following: result.following,
-                followed: result.followers
+                followedrs: result.followers
             });
         })
         .catch(err => {
